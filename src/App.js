506c45deaffae9ac25a240ce54ca98e5f9bd79fc
huoran <303176152@qq.com>
2019-06-27 22:23:19 +0800

    submit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {observer} from 'mobx-react'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { observer } from "mobx-react";
 
 // 装饰器语法
 @observer
